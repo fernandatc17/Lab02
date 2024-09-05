97395c79a92c6712e84476fab3a3dd5bee09c5f3
fernandatc17 <abigailticlavilca46@gmail.com>
2024-09-04 20:46:02 -0500

    Responsable commit 1

--- src/main/java/pe/edu/tecsup/lab02/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab02/StudentController.java
@@ -1,7 +1,29 @@
 package pe.edu.tecsup.lab02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentController {
-    public void controller(){
-        //TO DO
+    private List<Student> students;
+
+    public StudentController() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    public Student getStudentById(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
     }
 }
